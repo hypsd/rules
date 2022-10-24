f1e1c82efb8e3e6bf4733c17aeb6f1fb334c2ad1
hypsd <66355082+hypsd@users.noreply.github.com>
2022-10-24 12:00:20 +0800

    Update simplypiano.js

--- simplypiano.js
+++ simplypiano.js
@@ -3,20 +3,19 @@
  *
 脚本功能：拦截100解锁超级会员
 软件版本：3.0.2【最高支持版本】
-下载地址：<a href='http://t.cn/A6MLFAJf' target='_blank'>http://t.cn/A6MLFAJf</a>
+下载地址：<a href='<a href='<a href='http://t.cn/A6MLFAJf' target='_blank'>http://t.cn/A6MLFAJf</a>' target='_blank'>http://t.cn/A6MLFAJf</a>' target='_blank'>http://t.cn/A6MLFAJf</a>
 脚本作者：Hausd0rff
 更新时间：2022-02-15
-电报频道：<a href='https://t.me/yqc_123' target='_blank'>https://t.me/yqc_123</a>
-问题反馈：<a href='https://t.me/yqc_777' target='_blank'>https://t.me/yqc_777</a>
+电报频道：<a href='<a href='<a href='https://t.me/yqc_123' target='_blank'>https://t.me/yqc_123</a>' target='_blank'>https://t.me/yqc_123</a>' target='_blank'>https://t.me/yqc_123</a>
+问题反馈：<a href='<a href='<a href='https://t.me/yqc_777' target='_blank'>https://t.me/yqc_777</a>' target='_blank'>https://t.me/yqc_777</a>' target='_blank'>https://t.me/yqc_777</a>
 使用声明：⚠️此脚本仅供学习与交流，
         请勿转载与贩卖！⚠️⚠️⚠️
 *******************************
 
 [rewrite_local]
 
-^https?:\/\/asla\.joytunes\.com\/443\/server\/asla\/accounts\/* url script-response-body <a href='https://raw.githubusercontent.com/hypsd/rules/main/simplypiano.js' target='_blank'>https://raw.githubusercontent.com/hypsd/rules/main/simplypiano.js</a>
+^https?:\/\/asla\.joytunes\.com\/443\/server\/asla\/accounts\/* url script-response-body https://raw.githubusercontent.com/hypsd/rules/main/simplypiano.js 
 [mitm] 
-
 hostname = *.joytunes.*
 *
 *
