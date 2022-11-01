48f9422a6f5ed460d04385f6b805662f9a06c9a3
hypsd <66355082+hypsd@users.noreply.github.com>
2022-11-01 12:25:39 +0800

    Update endel.js

--- endel.js
+++ endel.js
@@ -1,12 +1,11 @@
 
 
 [rewrite_local]
-^https?:\/\/api-production\.endel\.io\/v1\/user*? url https://raw.githubusercontent.com/hypsd/rules/main/endel.js
+^https?:\/\/api-production\.endel\.io\/v1\/user*? url <a href='https://raw.githubusercontent.com/hypsd/rules/main/endel.js' target='_blank'>https://raw.githubusercontent.com/hypsd/rules/main/endel.js</a>
 
 [mitm]
 hostname = api-production.endel.io
 
-*******************************/
 var body=$response.body;
 body = body.replace(/subscription_info\":\{[^\}]+\}/g,'subscription_info":{"type":"IOS","valid_until":1787191299.0716028}');
 $done(body);
