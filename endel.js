e392481d47cfff97ef224d6f64b0e5bdb32a32c0
hypsd <66355082+hypsd@users.noreply.github.com>
2022-11-01 12:24:17 +0800

    Update endel.js

--- endel.js
+++ endel.js
@@ -1,14 +1,7 @@
-/******************************
-⚠️如果放远程，请把endel.js替换成运程链接⚠️
-  🧚🏻‍♂️作者：🍡魔法师、木木🍡
-  wx交流群：1077223830
-🫧脚本名称:Endel 专业放松睡眠 解锁订阅 A+
-🫧建议配合working copy一起食用
-✈️working copy下载地址<a href='https://apps.apple.com/app/id896694807' target='_blank'>https://apps.apple.com/app/id896694807</a>✈️
-*******************************
+
 
 [rewrite_local]
-^https?:\/\/api-production\.endel\.io\/v1\/user*? url script-response-body https://raw.githubusercontent.com/hypsd/rules/main/endel.js
+^https?:\/\/api-production\.endel\.io\/v1\/user*? url script-response-body <a href='https://raw.githubusercontent.com/hypsd/rules/main/endel.js' target='_blank'>https://raw.githubusercontent.com/hypsd/rules/main/endel.js</a>
 
 
 [mitm]
